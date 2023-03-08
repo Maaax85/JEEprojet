d037f2fc13ccf18caa18ed0e4f3acbdfc9b980f5
Maaax85 <maxenceledu5@gmail.com>
2023-03-08 12:19:31 +0100

    Initial Commit

---
+++ src/main/java/com/octest/beans/Etudiant.java
@@ -0,0 +1,63 @@
+package com.octest.beans;
+
+public class Etudiant {
+
+	private String nom;
+	private String prenom;
+	private String genre;
+	private String previousSite;
+	private String previousFormation;
+	
+	public Etudiant(String nom, String prenom, String genre, String previousSite, String previousFormation) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.genre = genre;
+		this.previousSite = previousSite;
+		this.previousFormation = previousFormation;
+	}
+	
+	public Etudiant(String[] infosEtu) {
+		if (infosEtu.length==5) {
+			this.nom = infosEtu[0];
+			this.prenom = infosEtu[1];
+			this.genre = infosEtu[2];
+			this.previousSite = infosEtu[3];
+			this.previousFormation = infosEtu[4];
+			
+			System.out.println("Etudiant : " + infosEtu[0]);
+		}
+	}
+	
+	//Getters and Setters
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getPreviousSite() {
+		return previousSite;
+	}
+	public void setPreviousSite(String previousSite) {
+		this.previousSite = previousSite;
+	}
+	public String getPreviousFormation() {
+		return previousFormation;
+	}
+	public void setPreviousFormation(String previousFormation) {
+		this.previousFormation = previousFormation;
+	}
+}
---
+++ src/main/java/com/octest/servlets/Test2.java
@@ -0,0 +1,172 @@
+package com.octest.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+import javax.servlet.http.Part;
+
+import com.octest.beans.Etudiant;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/Test2")
+@MultipartConfig
+public class Test2 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private ArrayList<Etudiant> etudiants;
+    
+    public static final int TAILLE_TAMPON = 10240;
+    public static final String CHEMIN_FICHIERS = "C:/Users/ledum/Documents"; // A changer
+    
+       
+    public Test2() {
+        super();
+        this.etudiants = new ArrayList<Etudiant>();
+    }
+    
+    public ArrayList<Etudiant> getEtudiants() {
+    	return this.etudiants;
+    }
+    
+    public void addEtudiant(Etudiant etudiant) {
+    	this.getEtudiants().add(etudiant); 
+    }
+    
+    public void removeEtudiant (Etudiant etudiant) {
+    	int indexEtudiant = this.getEtudiants().indexOf(etudiant);
+    	if(indexEtudiant != -1) {
+    		this.getEtudiants().remove(indexEtudiant);
+    		//TODO : Prévenir utilisateur succès de l'opération
+    	}
+    	else {
+    		//TODO : Prévenir utilisateur échec de l'opération
+    	}
+    }
+    
+    public void addEtudiants(HttpServletRequest request, String nomChamp, String nomFichier) throws IOException {
+    	File file = new File(nomChamp+nomFichier);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line = "";
+        String[] tempArr;
+        while((line = br.readLine()) != null) {
+           tempArr = line.split(",");
+           int index=0;
+           String[] infosEtu = new String[5];
+           
+           for(String tempStr : tempArr) {
+              infosEtu[index] = tempStr;
+              index++;
+           }
+           
+           this.addEtudiant(new Etudiant(infosEtu));
+        
+	       request.setAttribute(nomChamp, nomFichier);
+		}
+	    br.close();
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour2.jsp").forward(request, response);
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+    	String action = request.getParameter("action");
+        if (action != null) {
+            if (action.equals("boutonAddEtu")) {
+            	String nom = request.getParameter("nom");
+                String prenom = request.getParameter("prenom");
+                String genre = request.getParameter("genre");
+                String previousSite = request.getParameter("previousSite");
+                String previousFormation = request.getParameter("previousFormation");
+                
+                Etudiant etudiant = new Etudiant(nom, prenom, genre, previousSite, previousFormation);
+                this.addEtudiant(etudiant);
+                
+            } else if (action.equals("boutonLoadEtus")) {
+            	
+	        	try {
+	            	// On récupère le champ du fichier
+	            	Part part = request.getPart("fichier");
+	            	
+	            	// On vérifie qu'on a bien reçu un fichier
+	                String nomFichier = this.getNomFichier(part);
+	
+	                // Si on a bien un fichier
+	                if (nomFichier != null && !nomFichier.isEmpty()) {
+	                    String nomChamp = part.getName();
+	                    // Corrige un bug du fonctionnement d'Internet Explorer
+	                     nomFichier = nomFichier.substring(nomFichier.lastIndexOf('/') + 1)
+	                            .substring(nomFichier.lastIndexOf('\\') + 1);
+	
+	                    // On écrit définitivement le fichier sur le disque
+	                    ecrireFichier(part, nomFichier, CHEMIN_FICHIERS);
+	                     
+	                    // On récupère les infos du fichier et on créé les étudiants résultants
+	                    this.addEtudiants(request, CHEMIN_FICHIERS, nomFichier);
+	                }
+	            }
+	            catch ( Exception ePasDeFichier ){
+	            	System.out.println("Pas de fichier ou mauvais type de fichier fourni");
+	            }
+            }
+        }
+        
+        //TODO : Créer seconde page
+        
+        HttpSession session = request.getSession();
+        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+    }
+    
+    private void ecrireFichier( Part part, String nomFichier, String chemin ) throws IOException {
+        BufferedInputStream entree = null;
+        BufferedOutputStream sortie = null;
+        try {
+            entree = new BufferedInputStream(part.getInputStream(), TAILLE_TAMPON);
+            sortie = new BufferedOutputStream(new FileOutputStream(new File(chemin + nomFichier)), TAILLE_TAMPON);
+
+            byte[] tampon = new byte[TAILLE_TAMPON];
+            int longueur;
+            while ((longueur = entree.read(tampon)) > 0) {
+                sortie.write(tampon, 0, longueur);
+            }
+        } finally {
+            try {
+                sortie.close();
+            } catch (IOException ignore) {
+            }
+            try {
+                entree.close();
+            } catch (IOException ignore) {
+            }
+        }
+    }
+    
+    private static String getNomFichier( Part part ) {
+        for ( String contentDisposition : part.getHeader( "content-disposition" ).split( ";" ) ) {
+            if ( contentDisposition.trim().startsWith( "filename" ) ) {
+                return contentDisposition.substring( contentDisposition.indexOf( '=' ) + 1 ).trim().replace( "\"", "" );
+            }
+        }
+        return null;
+    }   
+
+}
